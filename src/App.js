0b0edc44928a2ae5474142994692c6a414be9324
John Calderaio <jcalderaio@gmail.com>
2016-11-01 20:33:56 -0400

    fetched users name, id

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'rea
 import { View, Text } from 'react-native';
 import { Header, styles } from './components/common'; // Actually importing from ./components/common/index.js, but index is automattic
 
-
 const user = [];
 
 class Login extends Component {
+
+  test() {
+    console.log('Name: ', user.name);
+    console.log('Id: ', user.id);
+  }
+
   render() {
     return (
       <View>
@@ -21,18 +26,20 @@ class Login extends Component {
               } else {
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
-                    fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=', data.accessToken)
+                    var api = 'https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + data.accessToken;
+                    fetch(api)
                     .then((response) => response.json())
                     .then((json) => {
-                      user.name = json.name;
-                      user.id = json.id;
-                      user.user_friends = json.friends;
-                      user.email = json.email;
-                      user.username = json.name;
-                      user.loading = false;
-                      user.loggedIn = true;
-                      console.log('User ID:', data.user);
-                      // user.avatar = setAvatar(json.id);
+                       user.name = json.name;
+                       user.id = json.id;
+                       this.test();
+                    //   user.user_friends = json.friends;
+                    //   user.email = json.email;
+                    //   user.username = json.name;
+                    //   user.loading = false;
+                    //   user.loggedIn = true;
+                    //   console.log('User ID:', data.user);
+                    //   // user.avatar = setAvatar(json.id);
                     })
                     .catch(() => {
                       reject('ERROR GETTING DATA FROM FACEBOOK')
@@ -42,35 +49,12 @@ class Login extends Component {
               }
             }
           }
-          //onLogoutFinished={() => alert('User logged out')}
+          onLogoutFinished={() => alert('User logged out')}
         />
       </View>
     );
   }
 }
-
-/*
-
-initUser(token) {
-  fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
-  .then((response) => response.json())
-  .then((json) => {
-    // Some user object has been set up somewhere, build that user here
-    user.name = json.name
-    user.id = json.id
-    user.user_friends = json.friends
-    user.email = json.email
-    user.username = json.name
-    user.loading = false
-    user.loggedIn = true
-    user.avatar = setAvatar(json.id)
-  })
-  .catch(() => {
-    reject('ERROR GETTING DATA FROM FACEBOOK')
-  })
-}
-*/
-
 
 class App extends Component {
 	render() {
@@ -79,6 +63,9 @@ class App extends Component {
 				<Header headerText="Facebook Login" />
 				<View style={styles.containerCenter}>
 					<Login />
+          <Text>
+            {user.name}
+          </Text>
 				</View>
 			</View>
 		);
