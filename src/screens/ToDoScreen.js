8e108dac787f48d7515aed6f962e8e2eda569c55
John Calderaio <jcalderaio@gmail.com>
2016-11-13 21:28:46 -0500

    finished styles todolist

--- src/screens/ToDoScreen.js
+++ src/screens/ToDoScreen.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Text, View, TouchableHighlight, TextInput, ListView } from 'react-native';
 import Firebase from 'firebase';
 import { Navigation } from 'react-native-navigation';
-import { styles } from '../components/common/styles';
-import { CardSection, Button } from '../components/common';
-
 
 
 var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -20,50 +17,99 @@ export default class ToDoScreen extends Component {
     };
   }
 
+  addTodo() {
+    if (this.state.newTodo !== '') {
+      var newItem = this.state.newTodo;
+      this.state.items.push(newItem);
+      this.setState({
+        dataSource: ds.cloneWithRows(this.state.items),
+        newTodo: ''
+      });
+    }
+  }
+
   _renderRow(rowData) {
-  	return (<View style={{ height: 55, borderBottomColor: '#ededed', borderBottomWidth: 1,
-            paddingLeft:10, paddingTop:10}}>
-            	<Text style={{ fontSize:16 }}>{rowData}</Text>
+  	return (<View
+              style={{
+                height: 55,
+                borderBottomColor: '#ededed',
+                borderBottomWidth: 1,
+                paddingLeft: 10,
+                paddingTop: 10 }}
+            >
+            <Text style={{ fontSize: 16 }}>
+              {rowData}
+            </Text>
             </View>
-           )
+          );
   }
 
   render() {
     return (
-    <View style={{ marginTop:60 }}>
-
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(text) => this.setState({ text })}
-        value={this.state.text}
-      />
-     <Text>{this.state.text}</Text>
-     <CardSection>
-       <Button
-         onPress={() => {
-           var newItem = this.state.text;
-           this.state.items.push(newItem);
-            this.setState({
-            	dataSource: ds.cloneWithRows(this.state.items),
-              text: ''
-            })
-
-         }}
-
-       >
-         Add Item
-       </Button>
-     </CardSection>
-
+    <View style={styles.appContainer}>
+      <View style={styles.inputcontainer}>
+        <TextInput style={styles.input}
+                   onChangeText={(text) => this.setState({newTodo: text})}
+                   value={this.state.newTodo}
+                   placeholder='+ Add Task'
+        />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => this.addTodo()}
+          underlayColor='#dddddd'
+        >
+          <Text style={styles.btnText}>Add</Text>
+        </TouchableHighlight>
+      </View>
       	<ListView
       dataSource={this.state.dataSource}
       renderRow={this._renderRow}
-    />
+       />
     </View>
   );
   }
 };
 
+const styles = {
+  appContainer: {
+    flex: 1
+  },
+  inputcontainer: {
+    marginTop: 5,
+    padding: 10,
+    flexDirection: 'row'
+  },
+  btnText: {
+    fontSize: 18,
+    color: 'steelblue',
+    marginTop: 6,
+    marginRight: 25,
+    marginLeft: 20,
+
+  },
+  input: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    borderRadius: 4
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 12,
+    height: 44
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  todoText: {
+    flex: 1,
+  }
+};
 
 /*
 import React, { Component } from 'react';
